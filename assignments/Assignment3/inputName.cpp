62e24b44061e5d09fc2d222e58e672f6f79d09f2
Tim Spires <timothy.s.spires@gmail.com>
2014-07-11 16:31:43 -0400

    Completed Assignment 3 Part 2

--- assignments/Assignment3/inputName.cpp
+++ assignments/Assignment3/inputName.cpp
@@ -1,25 +1,41 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
-void split(string target, char delimiter);
+vector<string> split(string target, char delimiter);
 int main() {
     string name;
     string firstName;
     string lastName;
     string middleInitial;
-    
+    vector<string> splitName; 
     cout << "Please enter your first and last name with middle initial." << endl;
-    cin >> name;
-    split(name,' '); 
+    getline(cin,name);
+    splitName = split(name,' ');
+    cout << string(20, '*') << endl;
+    if (splitName.size() == 3) {
+	cout << splitName.at(2) << ", " << splitName.at(0) << " " << splitName.at(1).substr(0,1) << endl;
+    }
+    else if (splitName.size() == 2) {
+	cout << splitName.at(1) << ", " << splitName.at(0) << endl;
+    }
 }
 
-void split(string target, char delimiter) {
+//Simple split implemenetation
+vector<string> split(string target, char delimiter) {
+    vector<string> result;
     string tmp;
     for (int i = 0; i < target.length(); i++) {
-        cout << i << endl;
-        cout << target.at(i) << endl;
-	
+	if (target.at(i) != delimiter) {
+		tmp += target.at(i);
+	}
+	else if (target.at(i) == delimiter) {
+		result.push_back(tmp);
+		tmp = "";
+	}
     }
+    result.push_back(tmp);
+    return result;
 }
