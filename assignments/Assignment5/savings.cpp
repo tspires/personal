e4f9a2596d1bc11b9210345c32a5847d3772003d
Tim Spires <timothy.s.spires@gmail.com>
2014-07-21 16:29:17 -0600

    getLinks.sh

---
+++ assignments/Assignment5/savings.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+double compound(double P, double i, double n);
+double requestDouble(string name);
+int main() {
+    double P = 0.0;
+    double i = 0.0;
+    double n = 0.0;
+    P = requestDouble("principal amount");
+    i = requestDouble("interest amount");
+    n = requestDouble("compounding period");
+    P = compound(P,i,n);
+    cout << "Your interest after " << n << " years would be " << P << " dollars." << endl;
+    return 0;
+}
+
+double requestDouble(string name) {
+    double input = 0.0;
+    cout << "Please enter the " << name << " : ";
+    cin >> input;
+    return input;
+}
+
+double compound(double P, double i, double n) {
+    if (n > 0) {
+        P = P + (P * i);
+        n--;
+        compound(P,i,n);
+    }
+    else {
+        return P;
+    }
+}
