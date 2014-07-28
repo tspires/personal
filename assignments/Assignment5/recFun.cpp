c83d96da510f97f541d6889089352b28e14f157e
Timothy Spires <spirest@flip2.engr.oregonstate.edu>
2014-07-27 22:26:52 -0700

    fibonacci

--- assignments/Assignment5/recFun.cpp
+++ assignments/Assignment5/recFun.cpp
@@ -2,7 +2,21 @@
 
 using namespace std;
 
-int main() {
-    
+int fibonacci(int n);
 
+int main() {
+    int x = 3;
+    cout << fibonacci(x);
+    for (int i = 0; i < x; i++){
+        cout << fibonacci(x);
+        x++;
+   }
+}
+int fibonacci(int n){
+    if ( n <= 1 && n >= 0)
+        return n;
+    else if ( n > 1 ) 
+        return (fibonacci(n - 1) + fibonacci(n - 2));
+    else 
+        return -1;
 }
