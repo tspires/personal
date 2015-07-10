7444621203e405739e02595a3c0b85b3fd63e894
EC2 Default User <ec2-user@ip-172-31-27-48.us-west-2.compute.internal>
2015-07-10 18:20:58 +0000

    Added c

---
+++ practice/c/hi.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	int age = 24;
+	float height = 76.1023;
+	double super_power = 12412.1231231;
+	char initial = 'A';
+	char first_name[ ] = "Zed";
+	char last_name[ ]  = "Shaw";
+	printf("My name is %s\n", first_name);
+	printf(" I am %d years old.\n", age);
+	return 0;
+}
